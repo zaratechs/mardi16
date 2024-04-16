5b18a26b45ffaee2e7c489bf2b32807f2d53f1c9
Réda Hamza <reda.hamza@collegemv.qc.ca>
2024-04-16 16:47:21 -0400

    Not

---
+++ toto/src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function NotFound() {
+    return (
+        <div>
+           
+                <h1>Oops!</h1>
+                <h4>It’s not you. It’s us. Give it another try, please.</h4>
+                <h5>404 - The Page can't be found</h5>
+            
+        </div>
+    )
+}
+
+export default NotFound
