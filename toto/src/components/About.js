10498f5470f7f5ee0668227aa2d109e832d8f74d
Réda Hamza <reda.hamza@collegemv.qc.ca>
2024-04-16 16:43:38 -0400

    Home et About

---
+++ toto/src/components/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>About 456221</div>
+    )
+}
+export default About
---
+++ toto/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>Home V1 </div>
+    )
+}
+
+export default Home
